caa4e40ba3d3f8b06309ea897d5e58912d331bf9
Y.Dziashkevich <Y.Dziashkevich@gmail.com>
2014-10-15 17:16:18 +0300

    add info

--- inc/views/header.php
+++ inc/views/header.php
@@ -5,6 +5,7 @@
     <title>Test SCAND</title>
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/popup.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div class="container">
